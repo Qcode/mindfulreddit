a07b9eeddf66e3d87aaa0b1bc2ac1e775ff618e9
Ross Evans <therealrossevans@gmail.com>
2018-07-16 23:10:38 -0400

    Improve date countdown

--- src/App.js
+++ src/App.js
@@ -6,8 +6,23 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { chooseSubreddits: true, subredditData: [], error: null };
+    this.state = {
+      chooseSubreddits: true,
+      subredditData: [],
+      error: null,
+      currentTime: Date.now(),
+    };
+
     this.handleSubredditRequests = this.handleSubredditRequests.bind(this);
+
+    if (parseInt(localStorage.getItem('timeUntilNextFetch'), 10) > Date.now()) {
+      this.state.chooseSubreddits = false;
+      this.state.subredditData = JSON.parse(localStorage.getItem('lastData'));
+    }
+
+    this.updateTime = this.updateTime.bind(this);
+
+    setInterval(this.updateTime, 10000);
   }
 
   handleSubredditRequests(subreddits) {
@@ -62,15 +77,50 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
-  render() {
-    const timeLeft =
-      parseInt(localStorage.getItem('timeUntilNextFetch'), 10) - Date.now();
+  getTimeLeft() {
+    const hoursFromNow = localStorage.getItem('timeUntilNextFetch');
+    const endTime = hoursFromNow
+      ? parseInt(hoursFromNow, 10)
+      : Date.now() + 1.08e7;
+    const timeLeft = endTime - this.state.currentTime;
+
+    if (timeLeft <= 0) {
+      return 'You may view more posts now. Refresh the page to do so.';
+    }
 
     const hours = Math.floor(
       (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
     );
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 
+    let hoursStatement = ` ${hours} hours`;
+
+    if (hours === 1) {
+      hoursStatement = ` ${hours} hour`;
+    } else if (hours === 0) {
+      hoursStatement = '';
+    }
+
+    if (minutes !== 0) {
+      hoursStatement += ' and';
+    }
+
+    let minutesStatement = ` ${minutes} minutes`;
+
+    if (minutes === 1) {
+      minutesStatement = ` ${minutes} minute`;
+    } else if (minutes === 0) {
+      minutesStatement = '';
+    }
+
+    return 'You may view more posts in' + hoursStatement + minutesStatement;
+  }
+
+  updateTime() {
+    this.setState({ currentTime: Date.now() });
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>consciousreddit.com</h1>
@@ -79,9 +129,7 @@ class App extends Component {
         ) : (
           <RetrievedPosts data={this.state.subredditData} />
         )}
-        <p>
-          Your will get more redidt time in {hours} hours, {minutes} minutes
-        </p>
+        {!this.state.chooseSubreddits && <p>{this.getTimeLeft()}</p>}
       </div>
     );
   }
