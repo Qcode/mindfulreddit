e5e2ddb1a461120d765d28325e8b0672ee1f0820
Ross Evans <therealrossevans@gmail.com>
2018-07-21 18:35:43 -0400

    Fix bug where time left displays multiple times

--- src/RetrievedPosts.js
+++ src/RetrievedPosts.js
@@ -3,16 +3,20 @@ import PostDetail from './PostDetail';
 
 function RetrievedPosts(props) {
   const generateKey = posts => posts.reduce((acc, cur) => acc + cur.id, '');
-  return props.data.map(subredditPosts => (
-    <div key={generateKey(subredditPosts)}>
-      <h1>
-        <span className="red">/r/</span>
-        {subredditPosts[0].subreddit}
-      </h1>
-      {subredditPosts.map(post => <PostDetail key={post.id} post={post} />)}
+  return (
+    <div>
+      {props.data.map(subredditPosts => (
+        <div key={generateKey(subredditPosts)}>
+          <h1>
+            <span className="red">/r/</span>
+            {subredditPosts[0].subreddit}
+          </h1>
+          {subredditPosts.map(post => <PostDetail key={post.id} post={post} />)}
+        </div>
+      ))}
       <p>{props.timeLeft}</p>
     </div>
-  ));
+  );
 }
 
 export default RetrievedPosts;
